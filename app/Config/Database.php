6e68807d6b1102fa37c9f17fad2c3f664d050c47
adrien <adrien.gonzalez@laplateforme.io>
2020-12-16 12:05:39 +0100

    BDD

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends \CodeIgniter\Database\Config
 		'hostname' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => 'API',
+		'database' => 'api_games_servers',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
