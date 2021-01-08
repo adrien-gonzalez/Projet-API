f24230fc82d5c45e0a36cc65d3daee001157d57f
Camilloni <nicolas.camilloni@gmail.com>
2021-01-08 09:02:05 +0100

    clean

--- app/Views/profil_picture.php
+++
@@ -1,4 +0,0 @@
-<form action="../users" enctype="multipart/form-data" method="POST">
-    <input type="file" id="profil_picture" name="profil_picture">
-    <button type="submit">Modifier image</button>
-</form>
