8a9788036673af7c5949f0d659a343ede54c3e50
mathilde.roussier <mathilde.roussier@laplateforme.io>
2021-01-18 16:21:41 +0100

    ajout ScrollView ParamsPage

--- React/pages/ParamsPage.jsx
+++ React/pages/ParamsPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Dimensions } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import CatParams from "../components/cat_params";
 
 const windowHeight = Dimensions.get("window").height;
@@ -8,29 +9,57 @@ const windowWidth = Dimensions.get("window").width;
 
 const ParamsPage = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.container_top}>
-        <Text style={styles.title}> Paramètres </Text>
+    <ScrollView style={{ height: "60%" }}>
+      <View style={styles.container}>
+        <View style={styles.container_top}>
+          <Text style={styles.title}> Paramètres </Text>
+        </View>
+        <View style={styles.container_BlocParams}>
+          <Text style={styles.titleParams}>Paramètres utilisateur</Text>
+          <CatParams
+            cat="Mes informations perso."
+            iconStart={require("../assets/icons/user.png")}
+            iconEnd={require("../assets/icons/arrow.png")}
+          />
+          <CatParams
+            cat="Mes serveurs"
+            iconStart={require("../assets/icons/gamepad.png")}
+            iconEnd={require("../assets/icons/arrow.png")}
+          />
+        </View>
+        <View style={styles.container_BlocParams}>
+          <Text style={styles.titleParams}>Paramètres de l'application</Text>
+          <CatParams
+            cat="Notifications"
+            iconStart={require("../assets/icons/bell.png")}
+            iconEnd={require("../assets/icons/arrow.png")}
+          />
+          <CatParams
+            cat="Apparences"
+            iconStart={require("../assets/icons/eye.png")}
+            iconEnd={require("../assets/icons/arrow.png")}
+          />
+          <CatParams
+            cat="Langues"
+            iconStart={require("../assets/icons/lang.png")}
+            iconEnd={require("../assets/icons/arrow.png")}
+          />
+        </View>
+        <View style={styles.container_BlocParams}>
+          <Text style={styles.titleParams}>Infos supplémentaires</Text>
+          <CatParams
+            cat="Confidentialités & Securité"
+            iconStart={require("../assets/icons/lock.png")}
+            iconEnd={require("../assets/icons/arrow.png")}
+          />
+          <CatParams
+            cat="Aide & Support"
+            iconStart={require("../assets/icons/audio.png")}
+            iconEnd={require("../assets/icons/arrow.png")}
+          />
+        </View>
       </View>
-      {/* <View> */}
-      <View style={styles.container_BlocParams}>
-        <Text style={styles.titleParams}>Paramètres utilisateur</Text>
-        <CatParams cat="Mes informations perso." iconStart={require('../assets/icons/user.png')} iconEnd={require('../assets/icons/arrow.png')} />
-        <CatParams cat="Mes serveurs" iconStart={require('../assets/icons/gamepad.png')} iconEnd={require('../assets/icons/arrow.png')}/>
-      </View>
-      <View style={styles.container_BlocParams}>
-        <Text style={styles.titleParams}>Paramètres de l'application</Text>
-        <CatParams cat="Notifications" iconStart={require('../assets/icons/bell.png')} iconEnd={require('../assets/icons/arrow.png')}/>
-        <CatParams cat="Apparences" iconStart={require('../assets/icons/eye.png')} iconEnd={require('../assets/icons/arrow.png')}/>
-        <CatParams cat="Langues" iconStart={require('../assets/icons/lang.png')} iconEnd={require('../assets/icons/arrow.png')}/>
-      </View>
-      <View style={styles.container_BlocParams}>
-        <Text style={styles.titleParams}>Infos supplémentaires</Text>
-        <CatParams cat="Confidentialités & Securité" iconStart={require('../assets/icons/lock.png')} iconEnd={require('../assets/icons/arrow.png')}/>
-        <CatParams cat="Aide & Support" iconStart={require('../assets/icons/audio.png')} iconEnd={require('../assets/icons/arrow.png')}/>
-      </View>
-      {/* </View> */}
-    </View>
+    </ScrollView>
   );
 };
 
@@ -40,7 +69,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     minHeight: windowHeight,
     justifyContent: "space-evenly",
-    backgroundColor:"#F1F1F1",
+    backgroundColor: "#F1F1F1",
   },
   container_top: {
     flex: 0.075,
@@ -61,7 +90,7 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: "bold",
     color: "#66A5F9",
-    width:windowWidth - 75,
+    width: windowWidth - 75,
   },
   title: {
     textAlign: "center",
