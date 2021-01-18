695182e02639f309ea20b3e679e6380f706c2ef2
Camilloni <nicolas.camilloni@gmail.com>
2021-01-18 13:44:44 +0100

    maj

--- React/App.js
+++ React/App.js
@@ -9,6 +9,9 @@ import TabComponent from './components/Tab';
 import TabGame from './components/TabGame';
 import ServersListPage from './pages/ServersListPage.jsx';
 import AddServerPage from './pages/AddServerPage.jsx';
+import ServerInfoPage from './pages/ServerInfoPage.jsx';
+import ResetPassword from './pages/ResetPassword.jsx';
+import ResetMail from './pages/ResetMail.jsx';
 import ParamsPage from './pages/ParamsPage';
 import ConnectPage from './pages/ConnectPage';
 import RegisterPage from './pages/RegisterPage';
@@ -34,7 +37,7 @@ export default function App() {
         <Tab.Screen name="ServersListPage" component={ServersListPage} options={{ tabBarButton: (props) => <TabComponent page='ServersListPage' icon='list' />}} />
         <Tab.Screen name="SelectGamePage" component={SelectGamePage} options={{ tabBarButton: (props) => <TabGame page='SelectGamePage' icon='home' />}} />
         <Tab.Screen name="AddServerPage" component={AddServerPage} options={{ tabBarButton: (props) => <TabComponent page='AddServerPage' icon='add' />}} />
-        <Tab.Screen name="ProfilePage" component={RegisterPage} options={{ tabBarButton: (props) => <TabComponent page='ProfilePage' icon='profile' />}} />
+        <Tab.Screen name="ProfilePage" component={ResetMail} options={{ tabBarButton: (props) => <TabComponent page='ProfilePage' icon='profile' />}} />
       </Tab.Navigator>
     </NavigationContainer>
   );
